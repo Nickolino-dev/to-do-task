cad724231f15e9c049abca4e3dcfbdd38d609257
Nicola La Gioia <nico.test@gmail.it>
2025-12-06 12:34:31 +0100

    creato array task

--- to-do-list/src/components/Tasks.jsx
+++ to-do-list/src/components/Tasks.jsx
@@ -1,13 +1,14 @@
 import Task from "./Task";
+import tasks from "../data/tasks.js";
 
 export default function Tasks() {
   return (
     <div>
       <ul>
         <Task></Task>
-        <Task></Task>
-        <Task></Task>
-        <Task></Task>
+        <Task text="Studia Javascript"></Task>
+        <Task text="Studia CSS"></Task>
+        <Task text="Studia Html"></Task>
       </ul>
     </div>
   );
---
+++ to-do-list/src/data/tasks.js
@@ -0,0 +1,24 @@
+let tasks = [
+  {
+    id: "123",
+    test: "Studia Html",
+    isCompleted: true,
+  },
+  {
+    id: "456",
+    test: "Studia CSS",
+    isCompleted: true,
+  },
+  {
+    id: "789",
+    test: "Studia Javascript",
+    isCompleted: true,
+  },
+  {
+    id: "987",
+    test: "Studia React",
+    isCompleted: true,
+  },
+];
+
+export default tasks;
